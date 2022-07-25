58a0f2d7189cb083c4feaf449e5ab0ab3d67f683
asapconet <aaronrickymeek@gmail.com>
2022-07-25 16:27:32 +0100

    Learing: react-hook- useEffect

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import { cleanup } from "@testing-library/react";
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [value, setValue] = useState(window.innerWidth);
+
+  const theListener = () => {
+    setValue(window.innerWidth);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    window.addEventListener("resize", theListener);
+    return () => {
+      console.log("cleanup");
+      window.removeEventListener("resize", theListener);
+    };
+  }, []);
+  console.log("rerender");
+  return (
+    <>
+      <h1>Window</h1>
+      <h2>{value}px</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users";
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
+  const [values, setValues] = useState([]);
+
+  const getValues = async () => {
+    const reply = await fetch(url);
+    const values = await reply.json();
+    setValues(values);
+    // console.log(values)
+  };
+
+  useEffect(() => {
+    getValues();
+  }, []);
+
+  return (
+    <>
+      <h3>github users </h3>
+      <ul className="users">
+        {values.map((users) => {
+          const { id, login, avatar_url, html_url } = users;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
 };
 
 export default UseEffectFetchData;
