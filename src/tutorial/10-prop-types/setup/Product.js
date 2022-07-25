d81fcce4fab15afdcd39166edfd416bfcc2451cc
asapconet <aaronrickymeek@gmail.com>
2022-07-25 16:36:34 +0100

    Learing: working with props

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import defaultImage from "../../../assets/default-image.jpeg";
 
-const Product = () => {
-  return <article className='product'>single product</article>;
+const Product = ({ image, price, name }) => {
+const imageCheck = image && image.url 
+
+  return (
+    <article className="product">
+      <img src={imageCheck || defaultImage} alt={name} />
+      <h4>{name}</h4>
+      <p>${price || 4.99}</p>
+    </article>
+  );
 };
+
+Product.propTypes = {
+  image: PropTypes.object.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+};
+
+// Product.defaultProps = {
+//   name: "default name",
+//   image: defaultImage,
+//   price: "44.5",
+// };
 
 export default Product;
