7d2d8d29b0bf0e60f4fd54279f8cb21caee81e78
asapconet <aaronrickymeek@gmail.com>
2022-07-25 16:35:02 +0100

    Learing: form inputs-controled input

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,79 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [users, setUsers] = useState([]);
+
+  const enteredValue = (z) => {
+    z.preventDefault();
+    if (firstName && lastName && email) {
+      const profile = {
+        id: new Date().getTime().toString,
+        firstName,
+        lastName,
+        email,
+      };
+      setUsers((users) => {
+        return [...users, profile];
+      });
+      setFirstName("");
+      setLastName("");
+      setEmail("");
+    } else {
+      console.log("incomplete/empty entry made");
+    }
+  };
+  return (
+    <>
+      <article>
+        <form className="form">
+          <div className="form-control">
+            <label htmlFor="firstName">First-Name: </label>
+            <input
+              type="text"
+              id="firstName"
+              value={firstName}
+              onChange={(z) => setFirstName(z.target.value)}
+            ></input>
+          </div>
+          <div className="form-control">
+            <label htmlFor="lastName">Last-Name: </label>
+            <input
+              type="text"
+              id="lastName"
+              value={lastName}
+              onChange={(z) => setLastName(z.target.value)}
+            ></input>
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              value={email}
+              onChange={(z) => setEmail(z.target.value)}
+            ></input>
+          </div>
+          <button type="submit" onClick={enteredValue}>
+            add user
+          </button>
+        </form>
+        {users.map((them, index) => {
+          const { id, firstName, lastName, email } = them;
+          return (
+            <div className="item" key={id}>
+              <h4>
+                {firstName} {lastName}
+              </h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
