63cbd8f4b7dda665832f0334f6de90a8f6e8f981
asapconet <aaronrickymeek@gmail.com>
2022-07-25 16:43:54 +0100

    Learing:  conditional rendering

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
   // const firstValue = text || 'hello world';
   // const secondValue = text && 'hello world';
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      <div>
+        <h1>{text || <h1>Peter Pipper</h1>}</h1>
+        <button className="btn" onClick={() => setIsError(!isError)}>
+          toogle error
+        </button>
+        <div>
+          {isError && <h2>Error...</h2>}
+          {isError ? (
+            <h4>you pressed the wrong button try again</h4>
+          ) : (
+            <h5>All is working well!!!</h5>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ShortCircuit;
--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </div>
+  );
+};
+
+const Item = () => {
+  const [value, setValue] = useState(window.innerWidth);
+
+  const theSize = (value) => {
+    setValue(window.innerWidth);
+  };
+  useEffect((value) => {
+    window.addEventListener("resize", theSize);
+    return () => {
+      window.removeEventListener("resize", theSize);
+    };
+  }, []);
+
+  return (
+    <div>
+      <h1>window</h1>
+      <h3>size; {value} px</h3>
+    </div>
+  );
 };
 
 export default ShowHide;
